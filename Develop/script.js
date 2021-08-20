a79a6f488d15506d223cbc33247bcab9a980016f
alexbrandon3 <alexlbrandon@gmail.com>
2021-08-19 20:48:13 -0400

    minor formatting fix

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,7 @@ function writePassword(arrLC, arrUC, arrNUM, arrSP) {
   guaranteedArr.length = 0;
   passwordArr.length = 0;
 }
+
 let charTotal;
 var availableCharArray = [];
 var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
